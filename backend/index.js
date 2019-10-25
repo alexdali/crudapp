cc942e1dbae2ff8c8cdcc67529b5c7bec365e11e
alexeytas <alexdali@gmail.com>
2019-10-25 16:05:49 +0600

    move mongoDb connect data to db.js

--- backend/index.js
+++ backend/index.js
@@ -7,41 +7,7 @@ import { ApolloServer } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import typeDefs from './graphql/typedef';
 import resolvers from './graphql/resolvers';
-// import uri from './mongodb/db';
-require('dotenv').config({ path: 'variables.env' });
-
-// Import GraphQL Schema
-// const schema = require('./schema')
-
-// make graphql schema
-// const schema = makeExecutableSchema({
-//   typeDefs,
-//   resolvers,
-// });
-
-
-// const {MongoClient} = require('mongodb');
-
-// const uri = 'mongodb+srv://dbprog:<password>@cluster0-yywck.mongodb.net/test?retryWrites=true&w=majority';
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect((err) => {
-//   const collection = client.db('test').collection('devices');
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-
-const password = process.env.DBUSER_PASS;
-
-const mongoDBuri = process.env.MONGODB_URL;
-
-// mongoose.connect(`mongodb+srv://dbprog:${password}@cluster0-yywck.mongodb.net/test?retryWrites=true&w=majority`);
-// mongoose.connect('mongodb://<dbuser>:<dbpassword>@<MongoDB URI>');
-
-// mongoose.connect(`mongodb+srv://dbprog:${password}@${mongoDBuri}`, { useNewUrlParser: true });
-
-const uri = `mongodb+srv://dbprog:${password}@${mongoDBuri}`;
-
+import uri from './mongodb/db';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -53,17 +19,6 @@ mongoose.connection.once('open', () => {
 // create server
 const app = express();
 const port = 8000;
-
-// add default route "/"
-// app.get("/", (req,res)=> {
-//   let query = `{
-//     person { name },
-//     people { name, description, }
-//   }`;
-//   graphql(schema, query).then(result => {
-//     res.json(result);
-//   });
-// });
 
 app.use(bodyParser.json());
 
--- backend/mongodb/db.js
+++ backend/mongodb/db.js
@@ -1,6 +1,9 @@
-// "mongodb+srv://dbprog:<password>@cluster0-yywck.mongodb.net/test?retryWrites=true&w=majority";
+require('dotenv').config({ path: 'variables.env' });
+
 const password = process.env.DBUSER_PASS;
 
-const uri = `mongodb+srv://dbprog:${password}@cluster0-yywck.mongodb.net/test?retryWrites=true&w=majority`;
+const mongoDBurl = process.env.MONGODB_URL;
+
+const uri = `mongodb+srv://dbprog:${password}@${mongoDBurl}`;
 
 export default uri;
