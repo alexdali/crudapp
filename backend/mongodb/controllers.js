6aa5f298ddf598cda3078da1c94e0beda3b1dbcb
alexeytas <alexdali@gmail.com>
2019-10-25 12:14:20 +0600

    update mongodb controllers.js from mint-branch

--- backend/mongodb/controllers.js
+++ backend/mongodb/controllers.js
@@ -6,10 +6,24 @@ const getUsers = async () => {
   return users;
 };
 
+// Get single User
+const getUser = async (req) => {
+  const { id } = req.params;
+  const user = await User.find({ userId: id });
+  return user;
+};
+
 // Get all posts
 const getPosts = async () => {
   const posts = await Post.find();
   return posts;
+};
+
+// Get single Post
+const getPost = async (req) => {
+  const { id } = req.params;
+  const post = await Post.find({ postId: id });
+  return post;
 };
 
 // Get all posts by User
@@ -24,4 +38,8 @@ const getCommentsByPost = async (req) => {
   const { id } = req.params;
   const comments = await Comment.find({ postId: id });
   return comments;
+};
+
+export {
+  getUsers, getUser, getPosts, getPost, getPostsByUser, getCommentsByPost,
 };
